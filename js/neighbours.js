1b464801ee320b772a67cca5aac555ae3e05b2c4
Mark Pavlovski <markpavlovski@gmail.com>
2018-01-13 19:09:43 -0800

    Fixed label coloring. Set the colors or assigned groups to correspond to the greyscale percentage relative to the max label

--- js/neighbours.js
+++ js/neighbours.js
@@ -196,106 +196,6 @@ class SingleLabelMatrix extends Matrix {
 
 }
 
-class MultiLabelMatrix extends Matrix {
-	constructor(maxtrix){
-		super(matrix.rows, matrix.columns);
-		this.inputData = matrix.data;
-		this.buffer = new Matrix(3);
-		this.parents = [];
-		this.layerData = [];
-		this.layerParents = [];
-	}
-	setBuffer(k,l,layer){
-		this.buffer.setToValue(null);
-		for (var i = 0; i < 3; i++){
-			for (var j = 0; j < 3; j++){
-				if ( k-1+i >= 0 && k-1+i <= this.rows -1  && l-1+j >= 0 && l-1+j <= this.columns -1 && this.inputData[k-1+i][l-1+j] >= layer){
-					this.buffer.data[i][j] = this.parents[this.data[k-1+i][l-1+j]];
-				}
-			}
-		}
-	}
-	isValidLocation(i,j,layer){
-		if ( i < 0 || j < 0 || i >= this.rows || j >= this.columns ){
-			return false;
-		} else if ( this.data[i][j] === null || this.data[i][j] === undefined ){
-			return false;
-		} else if ( this.inputData[i][j] < layer){
-			return false;
-		} else {
-			return true;
-		}
-	}
-	setLabels(){
-		// Step 1 - Assing Initial Labels
-		for (var layer = 1; layer < 3; layer++){
-			this.parents = [];
-			this.setToValue(null);
-			console.log(layer);
-			for (var i = 0; i < this.rows; i++){
-				for (var j = 0; j < this.columns; j++){
-					if (this.inputData[i][j] >= layer){
-						this.setBuffer(i,j,layer);
-						if (this.buffer.getMin() === null){
-							this.data[i][j] = this.parents.length;
-							this.parents.push(this.parents.length);
-						} else {
-							this.data[i][j] = this.parents[this.buffer.getMin()];
-							this.parents[this.data[i][j]] = this.parents[this.buffer.getMin()];
-							for (var m = 0; m < 3; m++){
-								for (var n = 0; n < 3; n++){
-									if ( this.isValidLocation(i-1+m, j-1+n, layer) ){
-										this.parents[this.parents[this.data[i-1+m][j-1+n]]] = this.parents[this.buffer.getMin()];
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			// Step 2 - Reassign Parents
-			for (var i=0; i < this.parents.length; i++){
-				while (this.parents[this.parents[i]] !== this.parents[i]){
-					this.parents[i] = this.parents[this.parents[i]];
-				}
-			}
-			// Step 3 - Relabel Parents
-			var uniqueParents = [];
-			for (var i=0; i < this.parents.length; i++){
-				if(uniqueParents.indexOf(this.parents[i]) === -1){
-					uniqueParents.push(this.parents[i])
-				}
-			}
-			for (var i=0; i < this.parents.length; i++){
-				this.parents[i] = uniqueParents.indexOf(this.parents[i])
-			}
-			// Step 4 - Store Parents
-			this.layerParents.push(this.parents);
-			this.layerData.push(this.data);
-		}
-	}
-
-	relabelMatrix(layer){
-		for (var i = 0; i < this.rows; i++){
-			for (var j = 0; j < this.columns; j++){
-				if (this.inputData[i][j] < layer){
-					this.data[i][j] = "-";
-				} else {
-					this.data[i][j] = this.parents[this.data[i][j]];
-				}
-			}
-		}
-	}
-	// getLabels method consolidates all steps into one and generates final labeled matrix.
-	getLabels(){
-		this.setLabels();
-		this.relabelMatrix(2);
-		var outputMatrix = new Matrix(this.rows,this.columns);
-		outputMatrix.data = this.data;
-		return outputMatrix;
-	}
-
-}
 
 
 class ShadedTable {
@@ -310,11 +210,17 @@ class ShadedTable {
 		this.HTMLString = "";
 	}
 	assignColor(){
-		var max = this.matrix.getMax();
-		var min = this.matrix.getMin();
+		var max = this.labelMatrix.getMax();
+		console.log(max)
+		var min = this.labelMatrix.getMin();
+		console.log(min)
 		for (var i =0; i< this.rows; i++){
 			for (var j =0; j< this.columns; j++){
-				this.colorMatrix.data[i][j] = 255-100*this.matrix.data[i][j]/(max-min);
+				if (typeof this.labelMatrix.data[i][j] === 'number'){
+					this.colorMatrix.data[i][j] = 230 - Math.floor(this.labelMatrix.data[i][j]/(max-min)*200);
+				} else {
+					this.colorMatrix.data[i][j] = 255
+				}
 			}
 		}
 	}
