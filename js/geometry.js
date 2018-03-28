6f20442c7e957af1941e077da716acb25479d140
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-27 17:10:25 -0700

    started playing with the elevations again

--- js/geometry.js
+++ js/geometry.js
@@ -3,26 +3,26 @@ function loadScene(){
 
 	//Stage standard variables
 	var scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xccffff );
+	scene.background = new THREE.Color( 0xffffff );
 
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight / 1.0, 1, 1000000);
-	
+
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight * 1.0);
-
-	var controls = new THREE.TrackballControls( camera );
-	controls.rotateSpeed = 1.0;
-	controls.zoomSpeed = 1.2;
-	controls.panSpeed = 0.8;
-	controls.noZoom = false;
-	controls.noPan = false;
-	controls.staticMoving = true;
-	controls.dynamicDampingFactor = 0.3;
+	//
+	// var controls = new THREE.TrackballControls( camera );
+	// controls.rotateSpeed = 1.0;
+	// controls.zoomSpeed = 1.2;
+	// controls.panSpeed = 0.8;
+	// controls.noZoom = false;
+	// controls.noPan = false;
+	// controls.staticMoving = true;
+	// controls.dynamicDampingFactor = 0.3;
 
 	containerThree = document.getElementById( 'canvas' );
 	document.body.appendChild( containerThree );
 	containerThree.appendChild( renderer.domElement );
-	
+
 	//Generate Mesh
 	var geometry = new THREE.Geometry();
 	var fullRow = tileLength * gridLength;
@@ -41,11 +41,11 @@ function loadScene(){
 		}
 
 
-		
+
 	  	// Define Faces
 		for (var j =  0; j < fullRow - 1; j++){
 		for (var i=0; i < fullRow- 1; i++){
-			//Facing Up    	
+			//Facing Up
 			geometry.faces.push( new THREE.Face3( fullRow * (j + 1) + i, fullRow*j + i+1, fullRow* j + i) )
 			geometry.faces.push( new THREE.Face3( fullRow* j + i+1,fullRow * (j + 1) + i, fullRow * (j + 1) + i + 1) )
 		}
@@ -57,18 +57,20 @@ function loadScene(){
 		geometry.computeFaceNormals()
 
 	// Setup scene
-	var material = new THREE.MeshLambertMaterial({color: 0x1EAEDB, wireframe: false}); //color: 0x1EAEDB vertexColors: THREE.VertexColors
+	// var material = new THREE.MeshLambertMaterial({color: 0x1EAEDB, wireframe: false}); //color: 0x1EAEDB vertexColors: THREE.VertexColors
+	var material = new THREE.MeshBasicMaterial({color: 0x3c5ea1, wireframe: true}); //color: 0x1EAEDB vertexColors: THREE.VertexColors
+
 	var cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
 
 	// Setup scene
-	camera.position.z = topHeight*1.3;    
-	camera.position.x = 0*cellSize;    
-	camera.position.y = 15*cellSize;  
-	camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), -0.5);      
+	camera.position.z = topHeight*1.3;
+	camera.position.x = 0*cellSize;
+	camera.position.y = 15*cellSize;
+	camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), -0.5);
 
 
-	var ambLight = new THREE.AmbientLight( 0x050101 ); // soft light
+	var ambLight = new THREE.AmbientLight( 0x010101 ); // soft light
 	scene.add( ambLight );
 
 	var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
@@ -80,7 +82,7 @@ function loadScene(){
 	clock = new THREE.Clock();
 	rotationAxis = new THREE.Vector3( 0, 1, 0 )
 	rotationSpeed = 2.5
-	
+
 
 	//Window resize
 	window.addEventListener( 'resize', onWindowResize, false );
@@ -88,11 +90,11 @@ function loadScene(){
 
  	// Render scene
 	function render() {
-		controls.update();
+		// controls.update();
 		requestAnimationFrame(render);
-		//cube.quaternion.setFromAxisAngle(rotationAxis, clock.getElapsedTime()/rotationSpeed);
+		cube.quaternion.setFromAxisAngle(rotationAxis, clock.getElapsedTime()/rotationSpeed);
 		renderer.render(scene, camera);
-	};	
+	};
 	render();
 
 	function onWindowResize() {
